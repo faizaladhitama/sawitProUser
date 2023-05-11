cefefa346e9a8bfef7fa271b7785e2d70bd3907e
Faizal Adhitama Prabowo <faizaladhitamaprabowo@gmail.com>
2023-05-11 22:33:19 +0700

    initial commit

---
+++ src/main/java/com/sawitPro/SawitProApplication.java
@@ -0,0 +1,13 @@
+package com.sawitPro;
+
+	import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SawitProApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SawitProApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sawitPro/SawitProApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sawitPro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SawitProApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
