2353d729e8e6ecfe4d5166d7eacde50477212686
Faizal Adhitama Prabowo <faizaladhitamaprabowo@gmail.com>
2023-05-14 22:44:48 +0700

    user repository

---
+++ src/main/java/com/sawitPro/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.sawitPro.repository;
+
+import com.sawitPro.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    @Query("Select u from User u Where u.phoneNumber = ?1")
+    User findUserByPhoneNumber(String phoneNumber);
+}
