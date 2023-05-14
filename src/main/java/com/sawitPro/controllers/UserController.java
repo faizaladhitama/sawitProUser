bc2dd1620d93657b488a0b7486ed79dcf3028154
Faizal Adhitama Prabowo <faizaladhitamaprabowo@gmail.com>
2023-05-14 22:45:26 +0700

    user controller

---
+++ src/main/java/com/sawitPro/controllers/UserController.java
@@ -0,0 +1,24 @@
+package com.sawitPro.controllers;
+
+import com.sawitPro.models.User;
+import com.sawitPro.repository.UserRepository;
+import com.sawitPro.services.UserService;
+import com.sawitPro.viewModels.Registration;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController("userControllerV1")
+@RequestMapping("/v1/user")
+public class UserController {
+    private final UserService userService;
+
+    UserController(UserService userService) {
+        this.userService = userService;
+    }
+    @PostMapping("/registration")
+    public Boolean registration(@RequestBody Registration registration){
+       return userService.registration(registration);
+    }
+}
