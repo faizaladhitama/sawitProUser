35d6a1d700f58f5319c5e46c95ba7b802745a9f3
Faizal Adhitama Prabowo <faizaladhitamaprabowo@gmail.com>
2023-05-14 22:44:13 +0700

    Input request

--- src/main/java/com/sawitPro/viewModels/Login.java
+++ src/main/java/com/sawitPro/viewModels/Login.java
@@ -1,5 +1,12 @@
 package com.sawitPro.viewModels;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Login {
     private String phoneNumber;
     private String password;
