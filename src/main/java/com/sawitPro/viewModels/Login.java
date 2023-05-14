41bfd6badbfb918f4f7261598984644b06a37f75
Faizal Adhitama Prabowo <faizaladhitamaprabowo@gmail.com>
2023-05-14 21:08:28 +0700

    Insert view models

---
+++ src/main/java/com/sawitPro/viewModels/Login.java
@@ -0,0 +1,6 @@
+package com.sawitPro.viewModels;
+
+public class Login {
+    private String phoneNumber;
+    private String password;
+}
---
+++ src/main/java/com/sawitPro/viewModels/Registration.java
@@ -0,0 +1,7 @@
+package com.sawitPro.viewModels;
+
+public class Registration {
+    private String phoneNumber;
+    private String name;
+    private String password;
+}
